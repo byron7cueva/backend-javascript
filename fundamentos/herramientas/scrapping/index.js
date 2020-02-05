abd3da5edbe7cec3f2429da8d62ac45d8aef56a5
byron7cueva <byron7cueva@gmail.com>
2020-02-05 05:50:25 -0500

    Fundamentos - Herramientas - Scraping

---
+++ fundamentos/herramientas/scrapping/index.js
@@ -0,0 +1,27 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  console.log('Lanzamos navgador!');
+  const browser = await puppeteer.launch();
+  // Se lance pero que se abra
+  // const browser = await puppeteer.launch({headless: false});
+
+  // Abrir una pagina
+  const page = await browser.newPage();
+  await page.goto('https://es.wikipedia.org/wiki/Node.js');
+
+  var titulo1 = await page.evaluate(() => {
+    // Lo siguiente se ejecuta en el navegador
+    const h1 = document.querySelector('h1');
+    console.log(h1.innerHTML);
+    console.log('mensaje');
+    return h1.innerHTML;
+  });
+
+  console.log(titulo1);
+
+
+  console.log('Cerramos el navegador');
+  browser.close();
+  console.log('Navegador cerrado');
+})();
