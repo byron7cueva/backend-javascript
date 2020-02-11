10f204594817f3fac74581218fb41f6fa399e3bd
byron7cueva <byron7cueva@gmail.com>
2020-02-11 18:41:31 -0500

    Node.js avanzado - bases de datos - implementando un modulo de base de datos

--- node-avanzado/platziverse-db/index.js
+++ node-avanzado/platziverse-db/index.js
@@ -1,6 +1,25 @@
 'use strict'
 
+const setupDatabase = require('./lib/db')
+const setupAgentModel = require('./models/agent')
+const setupMetricModel = require('./models/metric')
+
 module.exports = async function (config) {
+  const sequelize = setupDatabase(config)
+  const AgentModel = setupAgentModel(config)
+  const MetricModel = setupMetricModel(config)
+
+  // Relaciones
+  // Agente tiene muchas metrias
+  AgentModel.hasMany(MetricModel)
+  // Una metrica pertenece a un Agente
+  MetricModel.belognsTo(AgentModel)
+
+  await sequelize.authenticate()
+  // Si no hay las relaciones en la base de datos se crea por sequelize
+  // Tambien crea el id de las tablas y les asigna como clave primaria
+  // sequelize.sync()
+
   const Agent = {}
   const Metric = {}
 
