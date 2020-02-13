a13f06297ddded4d98c89be989e20c852de42468
Byron Cueva <byron7cueva@gmail.com>
2020-02-13 17:19:13 -0500

    Node.js avanzado - api - creando rutas

--- node-avanzado/platziverse-api/server.js
+++ node-avanzado/platziverse-api/server.js
@@ -2,9 +2,15 @@ const http = require('http')
 const express = require('express')
 const chalk = require('chalk')
 
+const api = require('./api')
+
 const port = process.env.PORT || 3000
 const app = express()
 const server = http.createServer(app)
+
+// Middlewares
+// Son funciones que se ejecutan antes de que la peticion llege a la ruta final
+app.use('/api', api)
 
 server.listen(port, () => {
   console.log(`${chalk.green('[platziverse-api]')} server listening on port ${port}`)
