ed2cfa3781054f81720519f94f215a1ecddf96b6
Byron Cueva <byron7cueva@gmail.com>
2020-02-13 17:34:54 -0500

    Node.js avanzado - api - manejando los errores de forma centralizado

--- node-avanzado/platziverse-api/api.js
+++ node-avanzado/platziverse-api/api.js
@@ -5,22 +5,22 @@ const { Router } = require('express')
 
 const api = Router()
 
-api.get('/agents', (req, res) => {
+api.get('/agents', (req, res, next) => {
   debug('A request has come to /agents')
   res.send({})
 })
 
-api.get('/agent/:uuid', (req, res) => {
+api.get('/agent/:uuid', (req, res, next) => {
   const { uuid } = req.params
   res.send({ uuid })
 })
 
-api.get('/metrics/uuid', (req, res) => {
+api.get('/metrics/uuid', (req, res, next) => {
   const { uuid } = req.params
   res.send({ uuid })
 })
 
-api.get('/metrics/:uuid:type', (req, res) => {
+api.get('/metrics/:uuid:type', (req, res, next) => {
   const { uuid, type } = req.params
   res.send({ uuid, type })
 })
--- node-avanzado/platziverse-api/server.js
+++ node-avanzado/platziverse-api/server.js
@@ -1,3 +1,4 @@
+const debug = require('debug')('platziverse:api')
 const http = require('http')
 const express = require('express')
 const chalk = require('chalk')
@@ -10,7 +11,27 @@ const server = http.createServer(app)
 
 // Middlewares
 // Son funciones que se ejecutan antes de que la peticion llege a la ruta final
+// Depende del orden en el cual se van agregando
 app.use('/api', api)
+
+// Express Error Handler
+app.use((error, req, res, next) => {
+  debug(`Error: ${error.message}`)
+  if (error.message.match(/not found/)) {
+    return res.status(404).send({error: error.message})
+  }
+
+  res.status(500).send({error: error.message})
+})
+
+function handleFatalError(error) {
+  console.error(`${chalk.red('[fatal error]')} ${error.message}`)
+  console.error(error.stack)
+  process.exit(1)
+}
+
+process.on('uncaughtException', handleFatalError)
+process.on('unhandledRejection', handleFatalError)
 
 server.listen(port, () => {
   console.log(`${chalk.green('[platziverse-api]')} server listening on port ${port}`)
