f08082d126959c38c0a484204e6ce7297df7f08c
Byron Cueva <byron7cueva@gmail.com>
2020-02-13 12:36:41 -0500

    Node.js avanzado - servidor mqtt - Almacenando agentes y metricas

--- node-avanzado/platziverse-mqtt/server.js
+++ node-avanzado/platziverse-mqtt/server.js
@@ -5,6 +5,7 @@ const mosca = require('mosca')
 const redis = require('redis')
 const chalk = require('chalk')
 const db = require('platziverse-db')
+const { parsePayload } = require('./utils')
 
 const backend = {
   type: 'redis',
@@ -19,27 +20,126 @@ const settings = {
   backend
 }
 
+const config = {
+  database: process.env.DB_NAME || 'platziverse',
+  username: process.env.DB_USER || 'desarrollo',
+  password: process.env.DB_PASS || 'desarrollo',
+  host: process.env.Db_HOST || '192.168.1.9',
+  dialect: 'postgres',
+  logging: message => debug(message)
+}
+
 const server = new mosca.Server(settings)
+const clients = new Map()
+let Agent, Metric
 
 // Cuando el servidor este listo
-server.on('ready', () => {
-  console.log(`${chalk.green('[platziverse-mqtt]')} server is running`);
+server.on('ready', async () => {
+  const services = await db(config).catch(handleFatalError)
+
+  Agent = services.Agent
+  Metric = services.Metric
+
+  console.log(`${chalk.green('[platziverse-mqtt]')} server is running`)
 })
 
 server.on('clientConnected', client => {
   debug(`Client Connected: ${client.id}`)
+  // Guardando el cliente
+  clients.set(client.id, null)
 })
 
-server.on('clientDisconnected', client => {
+server.on('clientDisconnected', async client => {
   debug(`Client Disconnected: ${client.id}`)
+  const agent = clients.get(client.id)
+  if (agent) {
+    agent.connected = false
+
+    try {
+      await Agent.createOrUpdate(agent)
+    } catch (error) {
+      handleError(error)
+    }
+
+    // Delete agent from list clients
+    clients.delete(client.id)
+
+    // Notificando que se desconecto
+    server.publish({
+      topic: 'agent/disconnected',
+      payload: JSON.stringify({
+        agent: {
+          uuid: agent.uuid
+        }
+      })
+    })
+    debug(`Client (${client.id}) associeted to Agent (${client.id}) marked disconnected`)
+  }
 })
 
-server.on('published', (packet, client) => {
+server.on('published', async (packet, client) => {
   // Tipo de mensaje
   debug(`Received: ${packet.topic}`)
-  // Que informacion nos ha llegado
-  debug(`Payload: ${packet.payload}`)
+  switch (packet.topic) {
+    case 'agent/connected':
+    case 'agent/disconnected':
+      // Que informacion nos ha llegado
+      debug(`Payload: ${packet.payload}`)
+      break
+    case 'agent/message': {
+      debug(`Payload: ${packet.payload}`)
+      const payload = parsePayload(packet.payload)
+
+      if (payload) {
+        payload.agent.connected = true
+        let agent
+        try {
+          agent = await Agent.createOrUpdate(payload.agent)
+        } catch (error) {
+          return handleError(error)
+        }
+
+        debug(`Agent ${agent.uuid} saved`)
+
+        // Notificar al agente
+        if (!clients.get(client.id)) {
+          clients.set(client.id, agent)
+          server.publish({
+            topic: 'agent/connected',
+            // El payload tiene que ser en string
+            payload: JSON.stringify({
+              agent: {
+                uuid: agent.uuid,
+                name: agent.name,
+                hostname: agent.hostname,
+                pid: agent.pid,
+                connected: agent.connected
+              }
+            })
+          })
+        }
+
+        // Store
+        for (const metric of payload.metrics) {
+          let m
+          try {
+            m = await Metric.create(agent.uuid, metric)
+          } catch (error) {
+            return handleError(error)
+          }
+
+          debug(`Metric ${m.id} saved on agent ${agent.uuid}`)
+        }
+      }
+      break
+    }
+  }
 })
+
+function handleError (error) {
+  console.error(`${chalk.red('[error]')} ${error.message}`)
+  console.error(error.stack)
+}
 
 // Manejando un error emitido por el servidor
 server.on('error', handleFatalError)
