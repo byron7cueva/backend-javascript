ae047206149dd580778c07e867b44cb0204a9b1e
Byron Cueva <byron7cueva@gmail.com>
2020-02-13 16:33:25 -0500

    Node.js avanzado - agente - emitiendo mensajes

--- node-avanzado/platziverse-agent/README.md
+++ node-avanzado/platziverse-agent/README.md
@@ -6,19 +6,35 @@
 const PlatziverseAgent = require('platziverse-agent')
 
 const agent = new PlatziverseAgent({
+  name: 'myapp',
+  username: 'admin',
   interval: 2000
+})
+
+agent.addMetric('rss', function getRss() {
+  return process.memoryUsage().rss
+})
+
+agent.addMetric('promiseMetric', function getRandomPromise () {
+  return Promise.resolve(Math.random())
+})
+
+agent.addMetric('callbackMetric', function getRandomCallback (callback) {
+  setTimeout(() => {
+    callback(null, Math.random())
+  }, 1000)
 })
 
 agent.connect()
 
 // This agent only
-agent.on('connected')
-agent.on('disconnected')
-agent.on('message')
+agent.on('connected', handler)
+agent.on('disconnected', handler)
+agent.on('message', handler)
 
-// 
-agent.on('agent/connected')
-agent.on('agent/disconnected')
+// Other agents
+agent.on('agent/connected', handler)
+agent.on('agent/disconnected', handler)
 agent.on('agent/message', payload => {
   console.log(payload);
 })
--- node-avanzado/platziverse-agent/index.js
+++ node-avanzado/platziverse-agent/index.js
@@ -4,6 +4,8 @@ const debug = require('debug')('platziverse:agent')
 const mqtt = require('mqtt')
 const defaults = require('defaults')
 const uuid = require('uuid')
+const os = require('os')
+const {promisify} = require('util')
 const {EventEmitter} = require('events');
 const {parsePayload} = require('./utils')
 
@@ -25,6 +27,15 @@ class PlatziverseAgent extends EventEmitter {
     this._timer = null
     this._client = null
     this._agentId = null
+    this._metrics = new Map()
+  }
+
+  addMetric (type, fn) {
+    this._metrics.set(type, fn)
+  }
+
+  removeMetric (type) {
+    this._metrics.delete(type)
   }
 
   connect () {
@@ -42,8 +53,43 @@ class PlatziverseAgent extends EventEmitter {
         this._started = true
         this.emit('connected', this._agentId)
 
-        this._timer = setInterval(() => {
-          this.emit('agent/message', 'this is a message')
+        this._timer = setInterval(async () => {
+          if (this._metrics.size > 0) {
+            let message = {
+              agent: {
+                uuid: this._agentId,
+                name: this._ops.name,
+                username: this._ops.username,
+                hostname: os.hostname() || 'localhost',
+                pid: process.pid
+              },
+              metrics: [],
+              timestamp: new Date().getTime()
+            }
+          }
+
+          // Destructurin
+          for (let [metric, fn] of this._metrics) {
+            // Functionariti: El tamaño de los argumentos de una funcion
+            if (fn.length === 1) {
+              // Si es callback entonces la convierto a una promesa
+              fn = promisify(fn)
+            }
+            message.metrics.push({
+              type: metric,
+              // Ejecuta una funcion estatico, o una promesa
+              value: await Promise.resolve(fn())
+            })
+          }
+
+          debug(`Sending`, message)
+
+          // Emitiendo al servisor mqtt
+          this._client.publish('agent/message', JSON.stringify(message))
+
+          // Emitiendo a los suscriptores del agente
+          this.emit('message', message)
+
         }, this._ops.interval)
       })
 
@@ -72,8 +118,9 @@ class PlatziverseAgent extends EventEmitter {
   disconnect () {
     if (this._started) {
       clearInterval(this._timer)
-      this.emit('disconnected')
+      this._client.end()
       this._started = false
+      this.emit('disconnected', this._agentId)
     }
   }
 }
