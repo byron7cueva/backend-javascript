4797fa78902d004c86f9610f5633e922fd31174d
Byron Cueva <byron7cueva@gmail.com>
2020-02-14 19:02:47 -0500

    Node.js avanzado - cli - Escuchando cuado se conecta un agente

--- node-avanzado/platziverse-cli/platziverse.js
+++ node-avanzado/platziverse-cli/platziverse.js
@@ -6,9 +6,13 @@
 
 const blessed = require('blessed')
 const contrib = require('blessed-contrib')
+const PlaziverseAgent = require('platziverse-agent')
 
 // Obteniendo la instancia de screen de blessed
+const agent = new PlaziverseAgent()
 const screen = blessed.screen()
+const agents = new Map()
+const agentMetrics = new Map()
 
 // Creando un grid
 const grid = new contrib.grid({
@@ -31,9 +35,50 @@ const line = grid.set(0, 1, 1, 3, contrib.line, {
   xPadding: 5
 })
 
+function renderData () {
+  const treeData = {}
+
+  for (let [ uuid, val ] of agents) {
+    const title = `${val.name} - (${val.pid})`
+    treeData[title] = {
+      uuid,
+      agent: true,
+      children: {}
+    }
+  }
+
+  // Asignando los datos al componente
+  tree.setData({
+    extended: true,
+    children: treeData
+  })
+
+  // Se necesita renderizar nuevamente la pantalla
+  screen.render()
+}
+
+agent.on('agent/connected', payload => {
+  const { uuid } = payload.agent
+  if (!agents.has(uuid)) {
+    agents.set(uuid, payload.agent)
+    agentMetrics.set(uuid, {})
+  }
+
+  renderData()
+})
+
+agent.on('agent/disconnected', payload => {
+
+})
+
+agent.on('agent/message', payload => {
+
+})
+
 // Capturar las teclas
 screen.key(['escape', 'q', 'C-c'], (ch, key) => {
   process.exit(0)
 })
 
+agent.connect()
 screen.render()
