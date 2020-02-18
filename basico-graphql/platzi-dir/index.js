c55c46efe334741e37bee22b36c74fc3842bc39f
Byron Cueva <byron7cueva@gmail.com>
2020-02-17 20:26:21 -0500

    Base GrahpQL - Configurando graphiql en modeo de desarrollo

--- basico-graphql/platzi-dir/index.js
+++ basico-graphql/platzi-dir/index.js
@@ -6,19 +6,24 @@ const express = require('express')
 const gqlMiddleware = require('express-graphql')
 const { readFileSync } = require('fs')
 const { join } = require('path')
+const cors = require('cors')
+
 const resolvers = require('./lib/resolvers')
 
 const app = express()
 const port = process.env.PORT || 3000
+const isDev = process.env.NODE_ENV !== 'production'
 
 // Definiendo el esquema
 const typeDefs = readFileSync(join(__dirname, 'lib', 'schema.graphql'), { encoding: 'utf8'})
 const schema = makeExecutableSchema({ typeDefs, resolvers })
 
+app.use(cors())
+
 app.use('/api', gqlMiddleware({
   schema,
   rootValue: resolvers,
-  graphiql: true
+  graphiql: isDev // Interface de graphiql para hacer queries
 }))
 
 app.listen(port, () => {
